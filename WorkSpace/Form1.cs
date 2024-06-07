7a4dc33be57d078e698c7baee5c6836dba831af1
mushroomvoodoo <normeeboy@gmail.com>
2024-06-07 14:10:07 +0300

    оки доки

--- WorkSpace/Form1.cs
+++ WorkSpace/Form1.cs
@@ -31,5 +31,20 @@ namespace WorkSpace
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
